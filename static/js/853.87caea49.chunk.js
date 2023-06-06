0291a0c6e806cd8f97c0cc9c5df6105bb745f86c
Oleh-Kliapko <goldclub1977@gmail.com>
2023-06-06 20:22:44 +0000

    Deploying to gh-pages from @ Oleh-Kliapko/GooseTrack_front@d6084bc9c7d746b8d51dc455fb0a24bfd9832af1 🚀

---
+++ static/js/853.87caea49.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkGooseTrack_front=self.webpackChunkGooseTrack_front||[]).push([[853],{3853:function(t,n,r){r.d(n,{_fJ:function(){return e}});var o=r(9983);function e(t){return(0,o.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"}},{tag:"polyline",attr:{points:"10 17 15 12 10 7"}},{tag:"line",attr:{x1:"15",y1:"12",x2:"3",y2:"12"}}]})(t)}}}]);
+//# sourceMappingURL=853.87caea49.chunk.js.map

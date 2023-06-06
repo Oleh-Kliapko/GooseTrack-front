f9cb85832edd2e935187944899e88a0903e9d69e
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-06-06 17:55:10 +0300

    create some func for date and edit PeriodPaginator

--- src/components/User/MainLayout/Calendar/CalendarToolbar/PeriodPaginator/PeriodPaginator.jsx
+++ src/components/User/MainLayout/Calendar/CalendarToolbar/PeriodPaginator/PeriodPaginator.jsx
@@ -1,41 +1,72 @@
+import PropTypes from 'prop-types';
+import {
+  ChooseDayBtn,
+  DateLabel,
+  WrapperPaginator,
+  WrapperPeriodBtn,
+} from './PeriodPaginator.styled';
 import { IconPag, PeriodBtn } from 'utils/Buttons/MainButton.styled';
-import { PeriodPaginatorContainer } from './PeriodPaginator.styled';
-import { useState } from 'react';
+import { format } from 'date-fns';
+import { getDateDetails } from 'helpers';
+// import { useState } from 'react';
 
 export const PeriodPaginator = ({ date, type, changeDate }) => {
-  const [buttonText, /* setButtonText */] = useState('JUNE 2023');
-  // прописати проптайпи для date, type, changeDate
+  // прописати проптайпи для date, type, changeDate - //!++++
 
-  // прописати логіку вибору buttonText залежно від type
+  // прописати логіку вибору buttonText залежно від type - //!++++
 
-  // прописати функцію кліку стрілочок - зміна дати/місяця і передача нової дати в changeDate
-  // якщо змінюється місяць, то в переданій даті змінюється тільки поле місяця, число лишається попереднім
-  
-  // стилізувати використовуючи styled.components
+  // прописати функцію кліку стрілочок - зміна дати/місяця і передача нової дати в changeDate - //!+/- зробив поки тільки лог передає у форматі 6 Jul 2023
+  // якщо змінюється місяць, то в переданій даті змінюється тільки поле місяця, число лишається попереднім //!++++
+
+  // стилізувати використовуючи styled.components//!++++
 
   // додаткова фіча:
   // продумати і прописати логіку выдкриття маленького календаря по кнопці з датою/місяцем
   // отримання з нього вибраної дати чи місяця в змінну і передача її в changeDate
 
-  // всі логічні перетворення одного формату дати в іншу можна виносити в окремий файл функції в хелперах
+  // всі логічні перетворення одного формату дати в іншу можна виносити в окремий файл функції в хелперах//!+/- поки тут бо так плутаюся
 
-/*   console.log(date);
-  const dateRecieved = new Date(date);
-  const monthDetails = changeDate(date);
-  const year = dateRecieved.getFullYear(); */
+  const data = getDateDetails(date);
+  const { selectDate, monthName, year } = data;
+  const dateFormated = date => format(date, 'd MMM yyyy');
+  const dateForView = dateFormated(selectDate);
+  const handlerClick = operation => {
+    const changedDate = dateFormated(changeDate(date, type, operation));
+    console.log(changedDate);
+  };
 
-  
   return (
-    <PeriodPaginatorContainer>
-      <button>
-          {buttonText}
-      </button>
-      <PeriodBtn onClick={() => console.log('a')}>
-        <IconPag id="left" />
-      </PeriodBtn>
-      <PeriodBtn onClick={() => console.log('b')} id="right">
-        <IconPag />
-      </PeriodBtn>
-    </PeriodPaginatorContainer>
+    // !!!!!!!!!!!!!!!!!!!!DELETED <div> WHEN PUSH IN MAIN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+    <div style={{ width: '768px', padding: '32px 32px' }}>
+      <WrapperPaginator>
+        <ChooseDayBtn
+          onClick={() => console.log('calls the calendar to select a date')}
+        >
+          <DateLabel dateTime={date} style={{ color: 'white' }}>
+            {type === 'month' ? (
+              <>
+                {monthName} {year}
+              </>
+            ) : (
+              <>{dateForView}</>
+            )}
+          </DateLabel>
+        </ChooseDayBtn>
+        <WrapperPeriodBtn>
+          <PeriodBtn onClick={() => handlerClick('decrease')}>
+            <IconPag id="left" />
+          </PeriodBtn>
+          <PeriodBtn onClick={() => handlerClick('increment')} id="right">
+            <IconPag />
+          </PeriodBtn>
+        </WrapperPeriodBtn>
+      </WrapperPaginator>
+    </div>
   );
+};
+
+PeriodPaginator.propTypes = {
+  date: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  changeDate: PropTypes.func.isRequired,
 };

1cc80cf9233c7a93b1d88bee107fd5c24fe526c4
AlenaMushko <evstafienko_a@ukr.net>
2023-06-07 03:10:41 +0300

    redux tasks work correctly

--- src/redux/tasks/selectors.js
+++ src/redux/tasks/selectors.js
@@ -1,3 +1,22 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectErrorTasks = state => state?.tasks?.error;
-export const selectIsLoadingTasks = state=> state?.tasks?.isLoading;
-export const selectAllTasks = state => state?.tasks?.tasks;
+export const selectIsLoadingTasks = state => state?.tasks?.isLoading;
+export const selectTasksCurrentMonth = state => state?.tasks?.tasksCurrentMonth;
+
+export const selectAllTasks = state => state?.tasks?.allTasks;
+
+const date = new Date();
+const currentDate = date.toISOString().slice(0, 10);
+
+export const selectTasksForCurrentDate = createSelector(
+  selectTasksCurrentMonth,
+  (tasksCurrentMonth) => {
+    const currentTasks = tasksCurrentMonth.filter((task) => {
+      const taskDate = new Date(task.date);
+      const taskDateStr = taskDate.toISOString().slice(0, 10);
+      return taskDateStr === currentDate;
+    });
+    return currentTasks;
+  },
+);

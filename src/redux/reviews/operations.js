52d56a15bf02e894b8e91b9ff75ac14418a02deb
AlenaMushko <evstafienko_a@ukr.net>
2023-06-07 09:46:13 +0300

    redux tasks after fix

--- src/redux/reviews/operations.js
+++ src/redux/reviews/operations.js
@@ -1,7 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-
 export const fetchReviews = createAsyncThunk(
   'reviews/fetchAll',
   async (_, thunkAPI) => {

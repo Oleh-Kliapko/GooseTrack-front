e526fcfe3b6b6fd87ae979b28cb1b67ca47e29f6
Oleh Kliapko <goldclub1977@gmail.com>
2023-06-07 11:34:26 +0300

    add dispatch for reviews and tasks
    + fix slice and operations about reviews

--- src/components/User/MainLayout/MainLayout.jsx
+++ src/components/User/MainLayout/MainLayout.jsx
@@ -1,9 +1,21 @@
 import { Outlet } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
 import { SideBar } from 'components/User/SideBar';
 import { Header } from 'components/User/Header';
 import { StyledContainer, StyledMain } from './MainLayout.styled';
 
+import { fetchOwnReviews } from '../../../redux/reviews/operations';
+import { fetchTasks } from '../../../redux/tasks/operations';
+
 export const MainLayout = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchOwnReviews());
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
   return (
     <StyledContainer>
       <SideBar />

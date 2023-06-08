6e06020afc7dc67f99838f767da116b24a4de4c9
maxyma-sanx <maxymasan.dev@gmail.com>
2023-06-08 17:47:10 +0300

    add slider visual

--- src/components/Main/ReviewsSlider/ReviewsSlider.styled.js
+++ src/components/Main/ReviewsSlider/ReviewsSlider.styled.js
@@ -61,10 +61,13 @@ export const ReviewsItem = styled.div`
   max-width: 335px;
   height: 194px;
 
-  border: 1px solid rgba(17, 17, 17, 0.1);
+  border: 2px solid ${({ theme }) => theme.colors.accent};
   border-radius: 8px;
 
-  overflow: hidden;
+  background-color: #ffffff;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'%3E%3Cdefs%3E%3ClinearGradient id='a' x1='0' x2='0' y1='1' y2='0'%3E%3Cstop offset='0' stop-color='%230FF'/%3E%3Cstop offset='1' stop-color='%23CF6'/%3E%3C/linearGradient%3E%3ClinearGradient id='b' x1='0' x2='0' y1='0' y2='1'%3E%3Cstop offset='0' stop-color='%23F00'/%3E%3Cstop offset='1' stop-color='%23FC0'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cg fill='%23FFF' fill-opacity='0' stroke-miterlimit='10'%3E%3Cg stroke='url(%23a)' stroke-width='2'%3E%3Cpath transform='translate(-127.05 1.5999999999999979) rotate(-6.350000000000001 1409 581) scale(0.8982089999999999)' d='M1409 581 1450.35 511 1490 581z'/%3E%3Ccircle stroke-width='4' transform='translate(-175.5 102) rotate(6.1 800 450) scale(1.002352)' cx='500' cy='100' r='40'/%3E%3Cpath transform='translate(13.400000000000006 -85.5) rotate(36.5 401 736) scale(1.002352)' d='M400.86 735.5h-83.73c0-23.12 18.74-41.87 41.87-41.87S400.86 712.38 400.86 735.5z'/%3E%3C/g%3E%3Cg stroke='url(%23b)' stroke-width='4'%3E%3Cpath transform='translate(612 8.2) rotate(-2.05 150 345) scale(0.9934029999999999)' d='M149.8 345.2 118.4 389.8 149.8 434.4 181.2 389.8z'/%3E%3Crect stroke-width='8' transform='translate(-138.5 -279.5) rotate(14.400000000000006 1089 759)' x='1039' y='709' width='100' height='100'/%3E%3Cpath transform='translate(-337.6 86.4) rotate(2.400000000000002 1400 132) scale(0.755)' d='M1426.8 132.4 1405.7 168.8 1363.7 168.8 1342.7 132.4 1363.7 96 1405.7 96z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+  background-attachment: fixed;
+  background-size: cover;
 
   @media (min-width: ${({ theme }) => theme.breakpoints.m}) {
     padding: 32px 32px 50px;
@@ -86,6 +89,8 @@ export const AuthorTop = styled.div`
 export const AuthorTopRight = styled.div`
   display: flex;
   flex-direction: column;
+  flex-grow: 1;
+  justify-content: center;
 `;
 
 export const AuthorPhoto = styled.img`

d201e0d4ee1b5d7bdd468f6f28699b2b308fc75a
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-08 12:18:18 +0300

    switching date on choosed day works correctly

--- src/components/User/MainLayout/Calendar/ChoosedDay/DayCalendarHead/DayCalendarHead.jsx
+++ src/components/User/MainLayout/Calendar/ChoosedDay/DayCalendarHead/DayCalendarHead.jsx
@@ -6,22 +6,50 @@ import {
   DateContainer,
 } from './DayCalendarHead.styled';
 import { useOutletContext } from 'react-router';
+import { getWeekDates } from 'helpers/getDataForWeek';
+import { useEffect, useState } from 'react';
+
+const getWeekNumber = (year, month, day) => {
+  let date = new Date(year, month - 1, day);
+  date.setHours(0, 0, 0, 0);
+  date.setDate(date.getDate() + 4 - (date.getDay() || 7));
+  let yearStart = new Date(date.getFullYear(), 0, 1);
+  let weekNo = Math.ceil(((date - yearStart) / 86400000 + 1) / 7);
+
+  return weekNo;
+}
+
+const getWeekDaysArray = (date) => {
+  const year = date.slice(0,4);
+  const month = date.slice(5,7);
+  const day = date.slice(8,10)
+  console.log(year);
+  console.log(month);
+  const week = getWeekNumber(parseInt(year, 10), parseInt(month, 10), parseInt(day, 10));
+  console.log(week);
+  const array = getWeekDates(parseInt(year, 10), parseInt(week, 10));
+  return array;
+};
 
 export function DayCalendarHead({ clickChooseDay }) {
   const [date, setDate] = useOutletContext();
+  const [days, setDays] = useState([]);
 
-  const numbersOfDays = [5, 6, 7, 8, 9, 10, 11];
+  useEffect(()=>{
+    const numbersOfDays = getWeekDaysArray(date);
+    console.log(date);
+    setDays(numbersOfDays);
+  }, [date])
+
   const dayNames = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']; // в майбутньому винести в окремий файл підтримки зміни мови
   const choosedNumberOfDayInNumberFormat = parseInt(date.slice(8,10));
   const makeCorrectFormatOfStringDate = (dayNumber) => {
     return `${date.slice(0,8)}${dayNumber.toString().padStart(2,0)}`
   }
-  console.log('here');
-  console.log(makeCorrectFormatOfStringDate(5));
   return (
     <Container>
       <DateWrapper>
-        {numbersOfDays.map((dayNumber, index) => {
+        {days.map((dayNumber, index) => {
           return(
             <WeekInfoWrapper>
             <DayOfWeek>{dayNames[index]}</DayOfWeek>

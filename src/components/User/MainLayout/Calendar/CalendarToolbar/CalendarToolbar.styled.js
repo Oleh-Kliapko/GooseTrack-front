0ca03e80b29967763ac4a1fad74dc1e0ea9ae3d0
YuriyRysak <yuriy.risak@gmail.com>
2023-06-08 18:02:06 +0300

    Update CalendarToolbar
    replacing styles in a component

--- src/components/User/MainLayout/Calendar/CalendarToolbar/CalendarToolbar.styled.js
+++ src/components/User/MainLayout/Calendar/CalendarToolbar/CalendarToolbar.styled.js
@@ -1,16 +1,30 @@
 import styled from 'styled-components';
 import { device } from 'styles/mediaVeriables';
 
-export const CalendarToolbarWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-bottom: 24px;
+// export const CalendarToolbarWrapper = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: flex-start;
+//   margin-bottom: 24px;
 
+//   @media ${device.tablet} {
+//     flex-direction: row;
+//     justify-content: space-between;
+//     align-items: center;
+//     margin-bottom: 32px;
+//   }
+// `;
+
+export const CalendarToolbarWrapper = styled.div`
+  margin-bottom: 24px;
+  background-color: ${({ theme }) => theme.colors.backgroundUserbar};
   @media ${device.tablet} {
+    display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
+  }
+  @media ${device.desktop} {
     margin-bottom: 32px;
   }
 `;

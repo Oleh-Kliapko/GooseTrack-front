55ae550e55b924c2bc5303fdf38f7d413416fdf4
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-09 11:05:09 +0300

    take tasks from data

--- src/components/User/MainLayout/Calendar/ChoosedDay/TaskForm/TaskForm.jsx
+++ src/components/User/MainLayout/Calendar/ChoosedDay/TaskForm/TaskForm.jsx
@@ -18,21 +18,37 @@ import { ReactComponent as Pencil } from "images/svg/pencil.svg";
 import { useSelector } from 'react-redux';
 import { selectCurrentTask, /* selectIsCurrentTaskEditing */ } from 'redux/tasks/selectors';
 
-export const TaskForm = ({ 
-    onSubmit, /* аргументом передавати об'єт такої ж структури, як приходить в taskDetails */
-    closeModal,
-    isEditing = false
-  }) => {
+export const TaskForm = ({ onSubmit, closeModal, isEditing = false }) => {
 
   const currentTask = useSelector(selectCurrentTask);
   // const isEditing = useSelector(selectIsCurrentTaskEditing);
-  console.log(currentTask);
+  // console.log(currentTask);
 
   const initialValues = {
     title: isEditing ? currentTask?.title : '',
     start: isEditing ? currentTask?.start : '',
     end: isEditing ? currentTask?.end : '',
     priority: isEditing ? currentTask?.priority : 'low',
+  };
+
+  const createTaskObject = (values) => {
+    return {
+      _id: currentTask._id,
+      title: values.title || '',
+      start: values.start || '00:00',
+      end: values.end || '00:00',
+      priority: values.priority || 'low',
+      date: currentTask.date,
+      category: values.category || 'to-do'
+    }
+  };
+
+  const addNewTask = (values) => {
+
+  };
+
+  const saveEditingTask = (values) => {
+    console.log(createTaskObject(values)); 
   };
 
   return (
@@ -49,7 +65,7 @@ export const TaskForm = ({
           isSubmitting,
           setFieldValue,
         }) => (
-          <StyledForm onSubmit={e => {e.preventDefault(); console.log(values)}}>
+          <StyledForm onSubmit={e => {e.preventDefault()}}>
             <Label htmlFor="title">
               <Span>Title</Span>
               <Input
@@ -97,7 +113,7 @@ export const TaskForm = ({
             </Wrapper>
 
             <RadioButtonGroup>
-              {['Low', 'Medium', 'High'].map(priority => (
+              {['low', 'medium', 'high'].map(priority => (
                 <RadioButtonLabel key={priority}>
                   <RadioButtonInput
                     type="radio"
@@ -137,7 +153,7 @@ export const TaskForm = ({
               </>)
 
               :
-                (<Button>
+                (<Button onClick={() => saveEditingTask(values)}>
                   <Pencil 
                   width="18"
                   height="18"

a199a3f528511bf619f5a9111a398451d28d0a51
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-06-09 19:09:12 +0300

    fix style in media
    moved the containers here and there until I found the right one, editing the calendar styles continues

--- src/components/User/MainLayout/Calendar/ChoosedMonth/CalendarTable/CalendarTable.styled.js
+++ src/components/User/MainLayout/Calendar/ChoosedMonth/CalendarTable/CalendarTable.styled.js
@@ -2,12 +2,10 @@ import styled from '@emotion/styled';
 
 export const CalendarTableContainer = styled.div`
   width: 100%;
-  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: stretch;
-  justify-content: space-between;
-  border-radius: 8px;
+  justify-content: flex-start;
 `;
 
 export const Week = styled.div`
--- src/components/User/MainLayout/Calendar/ChoosedMonth/MonthCalendarHead/MonthCalendarHead.styled.js
+++ src/components/User/MainLayout/Calendar/ChoosedMonth/MonthCalendarHead/MonthCalendarHead.styled.js
@@ -9,11 +9,12 @@ export const ListDay = styled.ul`
   display: flex;
   flex-direction: row;
   justify-content: space-around;
-  margin: 0;
-  padding: 0;
   margin-bottom: 14px;
   @media ${device.tablet} {
     margin-bottom: 18px;
+  }
+  @media ${device.desktop} {
+    margin-bottom: 15px;
   }
 `;
 

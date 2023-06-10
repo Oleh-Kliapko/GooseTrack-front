edab75a58973216e9f6e5f4477af24cdba99b31f
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-06-10 19:35:13 +0300

    small adjustment
    also went into the file with the structure but did not adjust it significantly, deleted the extra container, extracted the markup and array of days from the already existing variable

--- src/components/User/MainLayout/Calendar/ChoosedDay/DayCalendarHead/DayCalendarHead.styled.js
+++ src/components/User/MainLayout/Calendar/ChoosedDay/DayCalendarHead/DayCalendarHead.styled.js
@@ -1,26 +1,30 @@
 import styled from '@emotion/styled';
+import { device } from 'styles/mediaVeriables';
 
-export const Container = styled.div`
+export const DateWrapper = styled.ul`
   width: 100%;
   padding: 14px 40px;
   border-radius: 8px;
   margin-bottom: 14px;
   border: 1px solid ${({ theme }) => theme.colors.borderTableAndInput};
   background-color: ${({ theme }) => theme.colors.backgroundUserForm};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
-export const WeekInfoWrapper = styled.div`
+export const WeekInfoWrapper = styled.li`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
 `;
 
-export const DayOfWeek = styled.div`
+export const DayOfWeek = styled.p`
   text-align: center;
   font-weight: ${({ theme }) => theme.fontWeight.sb};
-  font-size: ${({ theme }) => theme.fontSizes.s};
-  line-height: 1.3;
+  font-size: ${({ theme }) => theme.fontSizes.m};
+  line-height: 1.1;
   color: ${({ id, theme }) => {
     switch (id) {
       case 5:
@@ -32,18 +36,31 @@ export const DayOfWeek = styled.div`
     }
   }};
   margin: 0px 0px 6px 0px;
-`;
+  overflow: hidden;
+  text-transform: uppercase;
 
-export const DateWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  @media ${device.tabletBefore} {
+    width: 10px;
+    /* :nth-of-type(1) {
+      width: 15px;
+    }
+    :nth-of-type(2) {
+      width: 12px;
+    }
+    :nth-of-type(3) {
+      width: 16px;
+    }
+    :nth-of-type(5) {
+      width: 9px;
+    } */
+  }
 `;
 
 export const DateContainer = styled.button`
   text-align: center;
   font-weight: ${({ theme }) => theme.fontWeight.b};
-  font-size: ${({ theme }) => theme.fontSizes.m};
+  font-size: ${({ theme }) => theme.fontSizes.xs};
+  line-height: 1.2;
   width: 27px;
   border-radius: 8px;
   background-color: ${({ picked, theme }) =>

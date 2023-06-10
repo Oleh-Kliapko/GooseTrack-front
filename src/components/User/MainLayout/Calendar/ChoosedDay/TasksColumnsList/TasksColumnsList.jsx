75c74c279f50dc7626831c897a18d53a6b84deb2
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-06-10 19:39:23 +0300

    added propKey child becauseReturn errorInConsole

--- src/components/User/MainLayout/Calendar/ChoosedDay/TasksColumnsList/TasksColumnsList.jsx
+++ src/components/User/MainLayout/Calendar/ChoosedDay/TasksColumnsList/TasksColumnsList.jsx
@@ -15,58 +15,65 @@ export const TasksColumnsList = () => {
     setIsTaskModalOpen(false);
   };
 
-  useEffect(()=>{
-    const monthNumber = parseInt(date.slice(5,7));
-    getTasksForOneMonth(monthNumber).then(response => {
-      const tasksArrayPerMonth = response?.data?.allTasks;
-      const tasksArrayPerDay = tasksArrayPerMonth.filter(task => task.date.slice(0,10) === date);
-      setDailyTasks(tasksArrayPerDay);
-    }).catch(error => console.log(error.message))
+  useEffect(() => {
+    const monthNumber = parseInt(date.slice(5, 7));
+    getTasksForOneMonth(monthNumber)
+      .then(response => {
+        const tasksArrayPerMonth = response?.data?.allTasks;
+        const tasksArrayPerDay = tasksArrayPerMonth.filter(
+          task => task.date.slice(0, 10) === date
+        );
+        setDailyTasks(tasksArrayPerDay);
+      })
+      .catch(error => console.log(error.message));
   }, [date]);
 
   const columns = [
     {
       title: 'To do',
-      category: 'to-do'
-    }, {
+      category: 'to-do',
+    },
+    {
       title: 'In progres',
-      category: 'in-progress'
-    }, {
+      category: 'in-progress',
+    },
+    {
       title: 'Done',
-      category: 'done'
-    }]; // зробити перевикористовуваним в ключі мови
+      category: 'done',
+    },
+  ]; // зробити перевикористовуваним в ключі мови
 
   return (
     <TasksColumnsListWrapper>
       {columns.map(column => {
-        return(
+        return (
           <TasksColumn
+            key={column.title}
             title={column.title}
             tasks={dailyTasks.filter(task => task.category === column.category)}
             setIsTaskModalOpen={setIsTaskModalOpen}
           />
-        )
+        );
       })}
 
-      {isTaskModalOpen && 
-        <TaskModal 
-          closeModal={closeTaskModal} 
+      {isTaskModalOpen && (
+        <TaskModal
+          closeModal={closeTaskModal}
           taskDetails={{
-            "_id": "string",
-            "title": "string",
-            "start": "string",
-            "end": "string",
-            "priority": "string",
-            "date": "2023-06-04T21:10:25.280Z",
-            "category": "string",
-            "owner": "string",
-            "createdAt": "2023-06-04T21:10:25.280Z"
+            _id: 'string',
+            title: 'string',
+            start: 'string',
+            end: 'string',
+            priority: 'string',
+            date: '2023-06-04T21:10:25.280Z',
+            category: 'string',
+            owner: 'string',
+            createdAt: '2023-06-04T21:10:25.280Z',
           }}
           isEditing={isTaskEditing}
           setIsEditing={setIsTaskEditing}
         />
-      }
-
+      )}
     </TasksColumnsListWrapper>
   );
 };

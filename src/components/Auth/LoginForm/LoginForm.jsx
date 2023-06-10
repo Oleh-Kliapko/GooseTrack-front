b84f6bf21848255b15c77287c458eef81b308c7d
alena <108529764+AlenaMushko@users.noreply.github.com>
2023-06-10 20:44:49 +0300

    fix

--- src/components/Auth/LoginForm/LoginForm.jsx
+++ src/components/Auth/LoginForm/LoginForm.jsx
@@ -1,15 +1,16 @@
 import { useFormik } from 'formik';
+import { useState } from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { useDispatch } from 'react-redux';
 import {
   StyledButton,
   StyledForm,
   StyledHeading,
   StyledIcon,
 } from './LoginForm.styled';
-import { useState } from 'react';
+
 import { AuthField } from '../AuthField/AuthField';
 import { validateLoginForm } from 'helpers';
-import { FiLogIn } from 'react-icons/fi';
-import { useDispatch } from 'react-redux';
 import { logIn } from '../../../redux/auth/operations';
 import { notification, useNotification } from 'helpers';
 
@@ -31,19 +32,19 @@ export const LoginForm = () => {
 
       console.log('res login ===>', payload);
 
-        if (payload === 'Request failed with status code 400') {
-          notification(toast, 'fail', 'Password or email is incorrect. Please check');
-          setPasswordValid(null);
-          formik.setFieldValue('password', '');
-        } else if (payload === 'Request failed with status code 403') {
-          notification(toast, 'fail', 'Email is not verified yet. Check email box for verification');
-          setPasswordValid(null);
-          formik.setFieldValue('password', '');
-        } else if (payload === 'Request failed with status code 404') {
-          notification(toast, 'fail', 'User is not found. Please check email');
-          setPasswordValid(null);
-          formik.setFieldValue('password', '');
-        }
+      if (payload === 'Request failed with status code 400') {
+        notification(toast, 'fail', 'Password or email is incorrect. Please check');
+        setPasswordValid(null);
+        formik.setFieldValue('password', '');
+      } else if (payload === 'Request failed with status code 403') {
+        notification(toast, 'fail', 'Email is not verified yet. Check email box for verification');
+        setPasswordValid(null);
+        formik.setFieldValue('password', '');
+      } else if (payload === 'Request failed with status code 404') {
+        notification(toast, 'fail', 'User is not found. Please check email');
+        setPasswordValid(null);
+        formik.setFieldValue('password', '');
+      }
     } catch (err){
       console.log('Error===>', err);
     }
@@ -94,3 +95,108 @@ export const LoginForm = () => {
     </StyledForm>
   );
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const LoginForm = () => {
+//   const dispatch = useDispatch();
+//   const [emailValid, setEmailValid] = useState(null);
+//   const [passwordValid, setPasswordValid] = useState(null);
+//
+//   const  toast = useNotification();
+//
+//   const onSubmitForm = async (values) => {
+//     try {
+//       // Validation of inputs
+//       const validationResponse = await validateLoginForm(values);
+//       setEmailValid(validationResponse.email);
+//       setPasswordValid(validationResponse.password);
+//
+//       const {payload} = await dispatch(logIn(values));
+//
+//       console.log('res login ===>', payload);
+//
+//         if (payload === 'Request failed with status code 400') {
+//           notification(toast, 'fail', 'Password or email is incorrect. Please check');
+//           setPasswordValid(null);
+//           formik.setFieldValue('password', '');
+//         } else if (payload === 'Request failed with status code 403') {
+//           notification(toast, 'fail', 'Email is not verified yet. Check email box for verification');
+//           setPasswordValid(null);
+//           formik.setFieldValue('password', '');
+//         } else if (payload === 'Request failed with status code 404') {
+//           notification(toast, 'fail', 'User is not found. Please check email');
+//           setPasswordValid(null);
+//           formik.setFieldValue('password', '');
+//         }
+//     } catch (err){
+//       console.log('Error===>', err);
+//     }
+//   };
+//
+//   const formik = useFormik({
+//     initialValues: {
+//       password: '',
+//       email: '',
+//     },
+//     onSubmit: values => {
+//       onSubmitForm(values);
+//     },
+//   });
+//
+//   return (
+//     <StyledForm onSubmit={formik.handleSubmit}>
+//       <StyledHeading>Log in</StyledHeading>
+//
+//       <AuthField
+//         name={'Email'}
+//         lableName={'Email'}
+//         value={formik.values.email}
+//         type={'email'}
+//         onChange={formik.handleChange}
+//         valid={emailValid?.valid}
+//         placeholder="Enter email"
+//         errorMessage={emailValid?.error}
+//       />
+//
+//       <AuthField
+//         name={'Password'}
+//         lableName={'Password'}
+//         value={formik.values.password}
+//         type={'text'}
+//         onChange={formik.handleChange}
+//         valid={passwordValid?.valid}
+//         placeholder="Enter password"
+//         errorMessage={passwordValid?.error}
+//       />
+//
+//       <StyledButton type="submit">
+//         Log in
+//         <StyledIcon>
+//           <FiLogIn size={17} color="#FFFFFF" />
+//         </StyledIcon>
+//       </StyledButton>
+//     </StyledForm>
+//   );
+// };
--- src/helpers/authFieldValidation.js
+++ src/helpers/authFieldValidation.js
@@ -1,4 +1,33 @@
 import * as yup from 'yup';
+
+ export const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Email must be valid email')
+    .required('Email is a required field'),
+  password: yup
+    .string()
+    .min(8, 'Password must be at least 8 characters')
+    .max(16, 'Password must be at most 16 characters')
+    .required('Password is a required field'),
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { patterns } from 'helpers/patterns';
 
 const nameSchema = yup

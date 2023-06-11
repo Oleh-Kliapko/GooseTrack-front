79a6cbcb0ff042cccc9655c69b94e1fb0fd16c1c
maxyma-sanx <maxymasan.dev@gmail.com>
2023-06-11 10:26:32 +0300

    add auth navigate btns

--- src/components/Auth/LoginForm/LoginForm.jsx
+++ src/components/Auth/LoginForm/LoginForm.jsx
@@ -1,8 +1,10 @@
-import { Formik,  ErrorMessage } from 'formik';
+import { Formik, ErrorMessage } from 'formik';
 import { useDispatch } from 'react-redux';
 import {
+  HeadingWrapper,
   StyledForm,
   StyledHeading,
+  StyledHomeBtn,
 } from './LoginForm.styled';
 import { StyledErrorMessage } from '../RegisterForm/RegisterForm.styled';
 import { AuthField } from '../AuthField/AuthField';
@@ -11,27 +13,39 @@ import { logIn } from '../../../redux/auth/operations';
 import { notification, useNotification } from 'helpers';
 import { MainBtn } from '../../../utils/Buttons/MainButton.styled';
 import { CgLogIn } from 'react-icons/cg';
+import { AiOutlineLeftCircle } from 'react-icons/ai';
 
 export const LoginForm = () => {
   const dispatch = useDispatch();
 
-  const  toast = useNotification();
+  const toast = useNotification();
 
   const onSubmitForm = async (values, { resetForm }) => {
     try {
-      const {payload} = await dispatch(logIn(values));
-      if (payload === 'Request failed with status code 400' || payload === 'Request failed with status code 401') {
-        notification(toast, 'fail', 'Password or email is incorrect. Please check');
+      const { payload } = await dispatch(logIn(values));
+      if (
+        payload === 'Request failed with status code 400' ||
+        payload === 'Request failed with status code 401'
+      ) {
+        notification(
+          toast,
+          'fail',
+          'Password or email is incorrect. Please check'
+        );
         return;
       } else if (payload === 'Request failed with status code 403') {
-        notification(toast, 'fail', 'Email is not verified yet. Check email box for verification');
+        notification(
+          toast,
+          'fail',
+          'Email is not verified yet. Check email box for verification'
+        );
         return;
       } else if (payload === 'Request failed with status code 404') {
         notification(toast, 'fail', 'User is not found. Please check email');
         return;
       }
       resetForm();
-    } catch (err){
+    } catch (err) {
       console.log('Error===>', err);
     }
   };
@@ -47,45 +61,48 @@ export const LoginForm = () => {
       validateOnChange={false}
       onSubmit={onSubmitForm}
     >
-      {({
-          values,
-          handleSubmit,
-          handleBlur,
-          handleChange,
-        }) => (
+      {({ values, handleSubmit, handleBlur, handleChange }) => (
+        <StyledForm onSubmit={handleSubmit}>
+          <HeadingWrapper>
+            <StyledHeading>Log in</StyledHeading>
+            <StyledHomeBtn to="/">
+              Home
+              <AiOutlineLeftCircle
+                style={{
+                  marginLeft: 6,
+                }}
+              />
+            </StyledHomeBtn>
+          </HeadingWrapper>
 
-    <StyledForm onSubmit={handleSubmit} >
-      <StyledHeading>Log in</StyledHeading>
+          <AuthField
+            name={'Email'}
+            lableName={'Email'}
+            value={values.email}
+            type={'email'}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            placeholder="Enter email"
+          />
+          <ErrorMessage component={StyledErrorMessage} name="email" />
 
-      <AuthField
-        name={'Email'}
-        lableName={'Email'}
-        value={values.email}
-        type={'email'}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        placeholder="Enter email"
-      />
-      <ErrorMessage component={StyledErrorMessage} name="email" />
+          <AuthField
+            name={'Password'}
+            lableName={'Password'}
+            value={values.password}
+            type={'text'}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            placeholder="Enter password"
+          />
+          <ErrorMessage component={StyledErrorMessage} name="password" />
 
-      <AuthField
-        name={'Password'}
-        lableName={'Password'}
-        value={values.password}
-        type={'text'}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        placeholder="Enter password"
-      />
-      <ErrorMessage component={StyledErrorMessage} name="password" />
-
-      <MainBtn style={{ width: '100%', marginTop:'32px' }} type="submit">
-        Sign up
-        <CgLogIn style={{ marginLeft: 11, width: 18, height: 18 }} />
-      </MainBtn>
-    </StyledForm>
+          <MainBtn style={{ width: '100%', marginTop: '32px' }} type="submit">
+            Sign up
+            <CgLogIn style={{ marginLeft: 11, width: 18, height: 18 }} />
+          </MainBtn>
+        </StyledForm>
       )}
     </Formik>
   );
 };
-
--- src/components/Auth/RegisterForm/RegisterForm.styled.js
+++ src/components/Auth/RegisterForm/RegisterForm.styled.js
@@ -1,56 +1,52 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 // check main font size and line height and delete doubling
 // check using font family
 
 export const StyledForm = styled.form`
-    position: relative;
-    box-sizing: border-box;
-    width: 100%;
-    min-height: 469px;
-    margin: auto 20px;
-    padding: 40px 20px;
-    display: flex;
-    flex-direction: column;
-    background-color: ${({theme}) => (theme.colors.white)};
-    border-radius: 8px;
-    font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+  position: relative;
+  box-sizing: border-box;
+  width: 100%;
+  min-height: 469px;
+  margin: auto 20px;
+  padding: 40px 20px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 8px;
+  font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+    'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
 
-    @media screen and (min-width: ${({theme}) => (theme.breakpoints.s)}){
-        width: 335px;
-    }
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.s}) {
+    width: 335px;
+  }
 
-    @media screen and (min-width: ${({theme}) => (theme.breakpoints.m)}){
-        width: 480px;
-        min-height: 521px;
-        padding: 40px;
-    }
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.m}) {
+    width: 480px;
+    min-height: 521px;
+    padding: 40px;
+  }
 `;
 
 export const StyledHeading = styled.h2`
-    margin-bottom: 8px;
-    color: ${({theme}) => (theme.colors.accent)};
-    font-size: 18px;
-    line-height: 24px;
-    font-weight: 600;
-    text-shadow: ${({theme}) => (theme.shadows.authHeading)};
-    text-align: start;
+  color: ${({ theme }) => theme.colors.accent};
+  font-size: 18px;
+  line-height: 24px;
+  font-weight: 600;
+  text-shadow: ${({ theme }) => theme.shadows.authHeading};
+  text-align: start;
 
-    @media screen and (min-width: ${({theme}) => (theme.breakpoints.m)}) {
-        margin-bottom: 22px;
-        font-size: 24px;
-    }
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.m}) {
+    font-size: 24px;
+  }
 `;
 
-export const StyledErrorMessage = styled.div `
+export const StyledErrorMessage = styled.div`
   font-weight: 400;
   font-size: 10px;
   line-height: 14px;
-  color: #DA1414;
+  color: #da1414;
   display: flex;
   flex-direction: column;
 `;
-
-

93a0e233672319354aece9c4234baf1a49ab2cb5
Bazykin11 <103375706+Bazykin11@users.noreply.github.com>
2023-06-11 18:19:21 +0300

    Update

--- src/components/User/SideBar/UserNav/UserNav.jsx
+++ src/components/User/SideBar/UserNav/UserNav.jsx
@@ -2,15 +2,20 @@ import { ReactComponent as UserIcon } from '../../../../images/svg/user-check.sv
 import { ReactComponent as CaledarIcon } from '../../../../images/svg/calendar-check.svg';
 import { UserNavWrap, UserNavTitle, UserNavItem } from './UserNav.styled';
 
-export const UserNav = () => {
+export const UserNav = ({ closeMobalMenu }) => {
+
+      const handleCloseMobalMenu = () => {
+        closeMobalMenu(false);
+      };
+  
   return (
     <UserNavWrap>
       <UserNavTitle>User Panel </UserNavTitle>
-      <UserNavItem to="/account">
+      <UserNavItem to="/account" onClick={handleCloseMobalMenu}>
         <UserIcon style={{ marginRight: 8, width: 20, height: 20 }} /> My
         account
       </UserNavItem>
-      <UserNavItem to="/calendar">
+      <UserNavItem to="/calendar" onClick={handleCloseMobalMenu}>
         <CaledarIcon style={{ marginRight: 8, width: 20, height: 20 }} />{' '}
         Calendar
       </UserNavItem>

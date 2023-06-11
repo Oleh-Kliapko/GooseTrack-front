8d5b0c9f8d90687aad17360e73dc8ec4f6b43791
alena <108529764+AlenaMushko@users.noreply.github.com>
2023-06-11 10:09:58 +0300

    fix imports

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -11,12 +11,12 @@ import { validateDate } from './validateDate';
 import { getWeekDetails } from './getWeekDetails';
 import { calendarType, checkCalendarType } from './checkCalendarType';
 import { getWeekDetailsByNumberOfWeek } from './getWeekDetailsByNumberOfWeek';
-// import {
-//   getPreviousWeekDate,
-//   getNextWeekDate,
-//   getPreviousMonthDate,
-//   getNextMonthDate,
-// } from './getChangedDate';
+import {
+  getPreviousWeekDate,
+  getNextWeekDate,
+  getPreviousMonthDate,
+  getNextMonthDate,
+} from './getChangedDate';
 import { getCurrentDate } from './getCurrentDate';
 import { userSchema } from './UserFormValidation';
 
@@ -32,6 +32,10 @@ export {
   getDateDetails,
   getWeekNumber,
   dayNamesArray,
+  getPreviousWeekDate,
+  getNextWeekDate,
+  getPreviousMonthDate,
+  getNextMonthDate,
   getWeekDetails,
   getWeekDetailsByNumberOfWeek,
   validateDate,

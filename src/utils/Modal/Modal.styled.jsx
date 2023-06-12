a400d46b9b4f28a64232c2bab3049e2897a1767d
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-12 14:19:52 +0300

    Update Modal.styled.jsx

--- src/utils/Modal/Modal.styled.jsx
+++ src/utils/Modal/Modal.styled.jsx
@@ -13,15 +13,15 @@ export const Backdrop = styled.div`
 
 export const Modal = styled.div`
   position: absolute;
-  border: 1px solid ${({ theme }) => theme.colors.borderTableAndInput};
-  box-shadow: 0px 4px 16px ${({ theme }) => theme.colors.borderDefaultColor};
+  border: 1px solid rgba(220, 227, 229, 0.8);
+  box-shadow: 0px 4px 16px rgba(17, 17, 17, 0.1);
   border-radius: 8px;
   max-width: 95%;
   padding: 48px 18px 40px;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  background-color: ${({ theme }) => theme.colors.backgroundUserForm};
+  background-color: ${({ theme }) => theme.colors.white};
 
   @media (min-width: 375px) {
     width: 300px;

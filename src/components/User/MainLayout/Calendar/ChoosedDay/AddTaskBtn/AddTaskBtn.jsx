2abda7d1c4ad36ced620a60f2d2ade4d0607dab1
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-12 14:05:44 +0300

    Update AddTaskBtn.jsx

--- src/components/User/MainLayout/Calendar/ChoosedDay/AddTaskBtn/AddTaskBtn.jsx
+++ src/components/User/MainLayout/Calendar/ChoosedDay/AddTaskBtn/AddTaskBtn.jsx
@@ -1,19 +1,37 @@
-import { Svg, AddTask } from './AddTaskBtn.styled';
+import {
+  AddTask,
+  Svg,
+} from './AddTaskBtn.styled';
 import icon from '../../../../../../images/svg/tasks.svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentTask, setIsCurrentTaskEditing, setIsTaskModalOpen } from 'redux/tasks/slice';
+import { selectChoosedDate } from 'redux/tasks/selectors';
 
-export const AddTaskBtn = ({
-  id,
-  getTypeOfColumn,
-  title,
-  setIsTaskModalOpen,
-  category,
-}) => {
+export const AddTaskBtn = ({category}) => {
+  const choosedDate = useSelector(selectChoosedDate);
+  const dispatch = useDispatch();
+  const openModal = () => {
+    dispatch(setIsTaskModalOpen(true));
+    dispatch(setIsCurrentTaskEditing(false));
+    dispatch(setCurrentTask(
+      {
+        _id: "",
+        title: "",
+        start: "00:00",
+        end: "00:00",
+        priority: "low",
+        date: choosedDate,
+        category: category
+      }
+    ))
+  };
+
   return (
-    <AddTask onClick={() => setIsTaskModalOpen(false, category)}>
-      <Svg>
-        <use xlinkHref={icon + '#icon-plus-add-button'}></use>
-      </Svg>{' '}
-      Add task
-    </AddTask>
+      <AddTask onClick={openModal  }>
+        <Svg>
+          <use xlinkHref={icon + '#icon-plus-add-button'}></use>
+        </Svg>{' '}
+        Add task
+      </AddTask>
   );
 };

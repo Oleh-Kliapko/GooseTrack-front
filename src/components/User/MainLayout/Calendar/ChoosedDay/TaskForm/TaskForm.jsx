2273d3d1513e05143daa05bbfb16ae1f8dedca41
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-06-12 20:47:07 +0300

    fix style

--- src/components/User/MainLayout/Calendar/ChoosedDay/TaskForm/TaskForm.jsx
+++ src/components/User/MainLayout/Calendar/ChoosedDay/TaskForm/TaskForm.jsx
@@ -8,24 +8,26 @@ import {
   RadioButtonGroup,
   RadioButtonInput,
   RadioButtonLabel,
-  Span,
   StyledForm,
   Button,
-  CancelBtn
 } from './TaskForm.styled';
-import { ReactComponent as Plus } from "images/svg/plus.svg";
-import { ReactComponent as Pencil } from "images/svg/pencil.svg";
+import { ReactComponent as Plus } from 'images/svg/plus.svg';
+import { ReactComponent as Pencil } from 'images/svg/pencil.svg';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectCurrentTask, selectIsCurrentTaskEditing } from 'redux/tasks/selectors';
+import {
+  selectCurrentTask,
+  selectIsCurrentTaskEditing,
+} from 'redux/tasks/selectors';
 import { addTask, updateTask } from 'redux/tasks/operations';
 import { notification, useNotification } from 'helpers';
 import { setIsTodayBusy } from 'redux/tasks/slice';
+import { SecondBtn, CancelBtn } from 'utils/Buttons/MainButton.styled';
 
-export const TaskForm = ({ onSubmit, closeModal}) => {
+export const TaskForm = ({ onSubmit, closeModal }) => {
   const isEditing = useSelector(selectIsCurrentTaskEditing);
   const currentDate = new Date().toISOString().slice(0, 10);
   const dispatch = useDispatch();
-  
+
   const toast = useNotification();
 
   const currentTask = useSelector(selectCurrentTask);
@@ -37,29 +39,28 @@ export const TaskForm = ({ onSubmit, closeModal}) => {
     priority: isEditing ? currentTask?.priority : 'low',
   };
 
-  const createTaskObject = (values) => {
-
+  const createTaskObject = values => {
     return {
       _id: currentTask._id,
       title: values.title || '',
       start: values.start || '00:00',
       end: values.end || '00:00',
       priority: values.priority || 'low',
-      date: currentTask.date.slice(0,10),
-      category: currentTask.category
-    }
+      date: currentTask.date.slice(0, 10),
+      category: currentTask.category,
+    };
   };
 
-  const addNewTask = (values) => {
-    if(values.title === '') {
+  const addNewTask = values => {
+    if (values.title === '') {
       notification(toast, 'fail', 'Task title can`t be empty');
       return;
-    };
-    if(values.start === '' ||  values.end === '') {
+    }
+    if (values.start === '' || values.end === '') {
       notification(toast, 'info', 'Please, set time');
       return;
     }
-    if(values.start >= values.end) {
+    if (values.start >= values.end) {
       notification(toast, 'fail', 'End time should be bigger than start time');
       return;
     }
@@ -68,19 +69,18 @@ export const TaskForm = ({ onSubmit, closeModal}) => {
       start: values.start,
       end: values.end,
       priority: values.priority || 'low',
-      date: currentTask.date.slice(0,10),
-      category: currentTask.category
+      date: currentTask.date.slice(0, 10),
+      category: currentTask.category,
     };
     dispatch(addTask(newTask));
-    if(currentTask.date.slice(0,10) === currentDate) {
+    if (currentTask.date.slice(0, 10) === currentDate) {
       dispatch(setIsTodayBusy(true));
-    };
+    }
     notification(toast, 'success', 'New task is successfully added');
     closeModal();
   };
 
-  const saveEditingTask = (values) => {
-    
+  const saveEditingTask = values => {
     const updatedTask = createTaskObject(values);
     dispatch(updateTask(updatedTask));
     closeModal();
@@ -88,8 +88,7 @@ export const TaskForm = ({ onSubmit, closeModal}) => {
 
   return (
     <>
-      <Formik 
-        initialValues={initialValues}>
+      <Formik initialValues={initialValues}>
         {({
           values,
           errors,
@@ -100,9 +99,13 @@ export const TaskForm = ({ onSubmit, closeModal}) => {
           isSubmitting,
           setFieldValue,
         }) => (
-          <StyledForm onSubmit={e => {e.preventDefault()}}>
+          <StyledForm
+            onSubmit={e => {
+              e.preventDefault();
+            }}
+          >
             <Label htmlFor="title">
-              <Span>Title</Span>
+              Title
               <Input
                 type="text"
                 name="title"
@@ -117,7 +120,7 @@ export const TaskForm = ({ onSubmit, closeModal}) => {
 
             <Wrapper>
               <Label htmlFor="start">
-                <Span>Start</Span>
+                Start
                 <Input
                   type="time"
                   step="60"
@@ -132,7 +135,7 @@ export const TaskForm = ({ onSubmit, closeModal}) => {
               </Label>
 
               <Label htmlFor="end">
-                <Span>End</Span>
+                End
                 <Input
                   type="time"
                   step="60"
@@ -165,40 +168,36 @@ export const TaskForm = ({ onSubmit, closeModal}) => {
             </RadioButtonGroup>
 
             <Wrapper>
-
-              {isEditing ?
-                  (<Button onClick={() => saveEditingTask(values)}>
-                    <Pencil 
-                    width="18"
-                    height="18"
-                    fill="none"
-                    stroke="#ffffff"
-                  />
-                    Edit
-                  </Button>)
-                :
-                (<>
-                  <Button aria-label='Button add' type="submit" onClick={() => addNewTask(values)}>
-                    <Plus 
-                      width="20"
-                      height="20"
-                      fill="none"
-                      stroke="#ffffff"
-                    />
+              {isEditing ? (
+                <Button onClick={() => saveEditingTask(values)}>
+                  <Pencil width="18" height="18" fill="none" stroke="#ffffff" />
+                  Edit
+                </Button>
+              ) : (
+                <>
+                  <SecondBtn
+                    aria-label="Button add"
+                    type="submit"
+                    onClick={() => addNewTask(values)}
+                    style={{ width: '50%' }}
+                  >
+                    <Plus width="20" height="20" fill="none" stroke="#ffffff" />
                     Add
-                  </Button>
-
+                  </SecondBtn>
                   <CancelBtn
-                    aria-label='Button cancel'
+                    aria-label="Button cancel"
                     type="button"
                     // disabled={isSubmitting}
-                    onClick={()=>{console.log('close'); closeModal()}}
+                    onClick={() => {
+                      console.log('close');
+                      closeModal();
+                    }}
+                    style={{ width: '50%' }}
                   >
                     Cancel
                   </CancelBtn>
-                </>)
-              }
-
+                </>
+              )}
             </Wrapper>
           </StyledForm>
         )}

874ac543b59e813e0715d884ee06373fa9075e2e
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-12 14:06:54 +0300

    Update ColumnHeadBar.jsx

--- src/components/User/MainLayout/Calendar/ChoosedDay/ColumnHeadBar/ColumnHeadBar.jsx
+++ src/components/User/MainLayout/Calendar/ChoosedDay/ColumnHeadBar/ColumnHeadBar.jsx
@@ -1,23 +1,15 @@
 import { AddTaskBtnHead } from '../AddTaskBtn/AddTaskBtnHead';
 import { TitleColumn, ColumnHeadWrapper } from './ColumnHeadBar.styled';
 
-export const ColumnHeadBar = ({
-  getTypeOfColumn,
-  title,
-  tasks,
-  setIsTaskModalOpen,
-  category,
-}) => {
+export const ColumnHeadBar = ({ getTypeOfColumn, title, tasks, setIsTaskModalOpen,category }) => {
   return (
     <ColumnHeadWrapper>
       <TitleColumn>{title}</TitleColumn>
-      <AddTaskBtnHead
-        tasks={tasks}
-        getTypeOfColumn={getTypeOfColumn}
-        title={title}
-        setIsTaskModalOpen={setIsTaskModalOpen}
-        category={category}
-      />
+      <div>
+        <AddTaskBtnHead
+          category ={category}
+        />
+      </div>
     </ColumnHeadWrapper>
   );
 };

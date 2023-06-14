8526bc4bd7de5b8ed5b876ec9c2b891ad62d7252
Oleh Kliapko <goldclub1977@gmail.com>
2023-06-14 21:09:49 +0300

    add Loader to UserForm

--- src/components/User/MainLayout/UserForm/UserForm.jsx
+++ src/components/User/MainLayout/UserForm/UserForm.jsx
@@ -2,11 +2,12 @@ import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Formik } from 'formik';
 import { useTranslation } from 'react-i18next';
-import { selectUser } from 'redux/auth/selectors';
+import { selectUser, selectIsRefreshingUser } from 'redux/auth/selectors';
 import { refreshUser, updateUser } from 'redux/auth/operations';
 import { validateUserForm } from 'helpers/UserFormValidation';
 import { UserField, BirthdayField } from '../UserField/UserField';
 import { notification, useNotification } from 'helpers';
+import { Loader } from 'utils/Loader';
 
 import { NewPasswordModal } from './NewPasswordModal/index.js';
 
@@ -29,6 +30,7 @@ import { MainBtn } from '../../../../utils/Buttons/MainButton.styled';
 
 export const UserForm = () => {
   const { user } = useSelector(selectUser);
+  const isRefreshing = useSelector(selectIsRefreshingUser);
   const dispatch = useDispatch();
 
   const [nameValid, setNameValid] = useState(null);
@@ -79,183 +81,200 @@ export const UserForm = () => {
   const onCloseModal = () => setIsShowModal(false);
 
   return (
-    <Wrapper>
-      <Formik
-        enableReinitialize={true}
-        initialValues={{
-          name: formData.name || user?.username || '',
-          email: formData.email || user?.email || '',
-          phone: formData.phone || user?.phone || '',
-          skype: formData.skype || user?.skype || '',
-          birthday:
-            newBirthday || formData.birthday || user?.birthday
-              ? new Date(newBirthday || formData.birthday || user?.birthday)
-              : new Date(),
-          avatarURL: formData.avatarURL || user?.avatarURL || '',
-        }}
-        onSubmit={async values => {
-          try {
-            const validationResponse = await validateUserForm(values);
+    <>
+      {isRefreshing && <Loader />}
 
-            setEmailValid(validationResponse.email);
-            setNameValid(validationResponse.name);
-            setPhoneValid(validationResponse.phone);
-            setSkypeValid(validationResponse.skype);
-            setBirthdayValid(validationResponse.birthday);
-            const checkValidResult = Object.values(validationResponse).every(
-              item => item.valid
-            );
+      <Wrapper>
+        {!isRefreshing && (
+          <Formik
+            enableReinitialize={true}
+            initialValues={{
+              name: formData.name || user?.username || '',
+              email: formData.email || user?.email || '',
+              phone: formData.phone || user?.phone || '',
+              skype: formData.skype || user?.skype || '',
+              birthday:
+                newBirthday || formData.birthday || user?.birthday
+                  ? new Date(newBirthday || formData.birthday || user?.birthday)
+                  : new Date(),
+              avatarURL: formData.avatarURL || user?.avatarURL || '',
+            }}
+            onSubmit={async values => {
+              try {
+                const validationResponse = await validateUserForm(values);
 
-            if (checkValidResult) {
-              const formData = new FormData();
-              formData.append('username', values.name);
-              formData.append('email', values.email);
-              if (values.phone) {
-                formData.append('phone', values.phone);
+                setEmailValid(validationResponse.email);
+                setNameValid(validationResponse.name);
+                setPhoneValid(validationResponse.phone);
+                setSkypeValid(validationResponse.skype);
+                setBirthdayValid(validationResponse.birthday);
+                const checkValidResult = Object.values(
+                  validationResponse
+                ).every(item => item.valid);
+
+                if (checkValidResult) {
+                  const formData = new FormData();
+                  formData.append('username', values.name);
+                  formData.append('email', values.email);
+                  if (values.phone) {
+                    formData.append('phone', values.phone);
+                  }
+                  if (values.skype) {
+                    formData.append('skype', values.skype);
+                  }
+                  formData.append('birthday', values.birthday);
+                  if (avatarURL) {
+                    formData.append('avatarURL', avatarURL);
+                  }
+
+                  dispatch(updateUser(formData));
+
+                  notification(
+                    toast,
+                    'success',
+                    t(`notifications.Profile change success`)
+                  );
+                }
+              } catch {
+                notification(
+                  toast,
+                  'fail',
+                  t(`notifications.Profile change error`)
+                );
               }
-              if (values.skype) {
-                formData.append('skype', values.skype);
-              }
-              formData.append('birthday', values.birthday);
-              if (avatarURL) {
-                formData.append('avatarURL', avatarURL);
-              }
+            }}
+          >
+            {({
+              values,
+              handleSubmit,
+              handleChange,
+              handleBlur,
+              dirty,
+              setFieldValue,
+            }) => (
+              <FormUser autoComplete="off" onSubmit={handleSubmit}>
+                <ContainerImg>
+                  {avatarURL ? (
+                    <ImgAvatar
+                      src={URL.createObjectURL(avatarURL)}
+                      alt="avatar"
+                    />
+                  ) : user?.avatarURL ? (
+                    <ImgAvatar src={user.avatarURL} alt="avatar" />
+                  ) : (
+                    <SvgAvatar>
+                      <IconUser />
+                    </SvgAvatar>
+                  )}
+                  <LabelImg htmlFor="avatarURL">
+                    <AddBtn />
+                    <InputFile
+                      id="avatarURL"
+                      type="file"
+                      onChange={e => {
+                        setFieldValue('avatarURL', e);
+                        setAvatarURL(e.target.files[0]);
+                      }}
+                      accept="image/*,.png,.jpg,.gif,.web"
+                      name="avatarURL"
+                    />
+                  </LabelImg>
+                </ContainerImg>
 
-              dispatch(updateUser(formData));
+                <UserName>{user?.username ? user?.username : ''} </UserName>
+                <User>{t(`accountPage.User`)}</User>
 
-              notification(toast, 'success', t(`notifications.Profile change success`));
-            }
-          } catch {
-            notification(toast, 'fail', t(`notifications.Profile change error`));
-          }
-        }}
-      >
-        {({
-          values,
-          handleSubmit,
-          handleChange,
-          handleBlur,
-          dirty,
-          setFieldValue,
-        }) => (
-          <FormUser autoComplete="off" onSubmit={handleSubmit}>
-            <ContainerImg>
-              {avatarURL ? (
-                <ImgAvatar src={URL.createObjectURL(avatarURL)} alt="avatar" />
-              ) : user?.avatarURL ? (
-                <ImgAvatar src={user.avatarURL} alt="avatar" />
-              ) : (
-                <SvgAvatar>
-                  <IconUser />
-                </SvgAvatar>
-              )}
-              <LabelImg htmlFor="avatarURL">
-                <AddBtn />
-                <InputFile
-                  id="avatarURL"
-                  type="file"
-                  onChange={e => {
-                    setFieldValue('avatarURL', e);
-                    setAvatarURL(e.target.files[0]);
+                <BlockInput>
+                  <UserField
+                    name={'Name'}
+                    lableName={t(`accountPage.Name`)}
+                    value={values.name}
+                    type={'name'}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    valid={nameValid?.valid}
+                    placeholder={t(`accountPage.Name`)}
+                    errorMessage={nameValid?.error}
+                  />
+
+                  <UserField
+                    name={'Phone'}
+                    lableName={t(`accountPage.Phone`)}
+                    value={values.phone}
+                    type={'tel'}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    valid={phoneValid?.valid}
+                    placeholder="+38"
+                    errorMessage={phoneValid?.error}
+                  />
+
+                  <BirthdayField
+                    name={'Birthday'}
+                    lableName={t(`accountPage.Birthday`)}
+                    value={values.birthday}
+                    type={'date'}
+                    input={true}
+                    maxDate={new Date()}
+                    selected={values.birthday}
+                    onChange={e => {
+                      setFieldValue('birthday', e);
+                      setNewBirthday();
+                      handleDatePicker();
+                    }}
+                    placeholder={t(`accountPage.Birthday`)}
+                    dateFormat="yyyy/MM/dd"
+                    open={isOpenDate}
+                    onClickOutside={() => setIsOpenDate(false)}
+                    onFocus={() => setIsOpenDate(true)}
+                    valid={birthdayValid?.valid}
+                    errorMessage={birthdayValid?.error}
+                  />
+
+                  <UserField
+                    name={'Skype'}
+                    lableName={'Skype'}
+                    value={values.skype}
+                    type={'text'}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    valid={skypeValid?.valid}
+                    placeholder={t(`accountPage.Add a skype number`)}
+                    errorMessage={skypeValid?.error}
+                  />
+
+                  <UserField
+                    name={'Email'}
+                    lableName={t(`accountPage.Email`)}
+                    value={values.email}
+                    type={'text'}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    valid={emailValid?.valid}
+                    placeholder={t(`accountPage.Email`)}
+                    errorMessage={emailValid?.error}
+                  />
+                </BlockInput>
+                <MainBtn type={'submit'} disabled={!dirty} padding="50">
+                  {t(`accountPage.Save changes`)}
+                </MainBtn>
+                <ChangePasswordBtn
+                  type={'button'}
+                  onClick={() => {
+                    setIsShowModal(true);
                   }}
-                  accept="image/*,.png,.jpg,.gif,.web"
-                  name="avatarURL"
-                />
-              </LabelImg>
-            </ContainerImg>
-
-            <UserName>{user?.username ? user?.username : ''} </UserName>
-            <User>{t(`accountPage.User`)}</User>
-
-            <BlockInput>
-              <UserField
-                name={'Name'}
-                lableName={t(`accountPage.Name`)}
-                value={values.name}
-                type={'name'}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                valid={nameValid?.valid}
-                placeholder={t(`accountPage.Name`)}
-                errorMessage={nameValid?.error}
-              />
-
-              <UserField
-                name={'Phone'}
-                lableName={t(`accountPage.Phone`)}
-                value={values.phone}
-                type={'tel'}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                valid={phoneValid?.valid}
-                placeholder="+38"
-                errorMessage={phoneValid?.error}
-              />
-
-              <BirthdayField
-                name={'Birthday'}
-                lableName={t(`accountPage.Birthday`)}
-                value={values.birthday}
-                type={'date'}
-                input={true}
-                maxDate={new Date()}
-                selected={values.birthday}
-                onChange={e => {
-                  setFieldValue('birthday', e);
-                  setNewBirthday();
-                  handleDatePicker();
-                }}
-                placeholder={t(`accountPage.Birthday`)}
-                dateFormat="yyyy/MM/dd"
-                open={isOpenDate}
-                onClickOutside={() => setIsOpenDate(false)}
-                onFocus={() => setIsOpenDate(true)}
-                valid={birthdayValid?.valid}
-                errorMessage={birthdayValid?.error}
-              />
-
-              <UserField
-                name={'Skype'}
-                lableName={'Skype'}
-                value={values.skype}
-                type={'text'}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                valid={skypeValid?.valid}
-                placeholder={t(`accountPage.Add a skype number`)}
-                errorMessage={skypeValid?.error}
-              />
-
-              <UserField
-                name={'Email'}
-                lableName={t(`accountPage.Email`)}
-                value={values.email}
-                type={'text'}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                valid={emailValid?.valid}
-                placeholder={t(`accountPage.Email`)}
-                errorMessage={emailValid?.error}
-              />
-            </BlockInput>
-            <MainBtn type={'submit'} disabled={!dirty} padding="50">
-            {t(`accountPage.Save changes`)}
-            </MainBtn>
-            <ChangePasswordBtn
-              type={'button'}
-              onClick={() => {
-                setIsShowModal(true);
-              }}
-              padding="0"
-            >
-              {t(`accountPage.Change password`)}
-            </ChangePasswordBtn>
-            {isShowModal && <NewPasswordModal onCloseModal={onCloseModal} />}
-          </FormUser>
+                  padding="0"
+                >
+                  {t(`accountPage.Change password`)}
+                </ChangePasswordBtn>
+                {isShowModal && (
+                  <NewPasswordModal onCloseModal={onCloseModal} />
+                )}
+              </FormUser>
+            )}
+          </Formik>
         )}
-      </Formik>
-    </Wrapper>
+      </Wrapper>
+    </>
   );
 };
-
-
